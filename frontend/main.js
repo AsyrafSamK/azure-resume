e82ece1cf1906ada09106cefb878784df9bfabc8
LAPTOP-7UL78SGI\aiman <aimanzairis60@gmail.com>
2024-01-15 03:39:15 +0800

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,13 @@ window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
 
-const functionApi ='http://localhost:7071/api/GetResumeCounter';
+
+const functionApiUrl = 'https://getresumecounter60.azurewebsites.net/api/GetResumeCounter?code=R5_m8O6fxl5ocXlkz7IEyUqtzTigsGSOIEKylyZJJCH_AzFuxaDUmA=='
+const localfunctionApi ='http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
